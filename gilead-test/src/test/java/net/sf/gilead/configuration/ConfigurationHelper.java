c2ef8ace03eb65eabaf7573cd78640756c84ef4a
Eduardo Matos de Souza <eduardo.souza@digitro.com.br>
2017-11-17 09:26:17 -0200

    Move code to test project.

--- gilead-test/src/test/java/net/sf/gilead/configuration/ConfigurationHelper.java
+++ gilead-test/src/test/java/net/sf/gilead/configuration/ConfigurationHelper.java
@@ -1,9 +1,6 @@
-/**
- * 
- */
 package net.sf.gilead.configuration;
 
-import net.sf.gilead.core.PersistenceUtil;
+import net.sf.gilead.core.IPersistenceUtil;
 import net.sf.gilead.core.PersistentBeanManager;
 import net.sf.gilead.core.serialization.JBossProxySerialization;
 import net.sf.gilead.core.store.NoProxyStore;
@@ -16,71 +13,67 @@ import net.sf.gilead.core.store.stateless.StatelessProxyStore;
  * @author bruno.marchesson
  */
 public class ConfigurationHelper {
-	// -------------------------------------------------------------------------
-	//
-	// Static helpers
-	//
-	// -------------------------------------------------------------------------
-	/**
-	 * Init bean manager for stateless mode
-	 */
-	public static PersistentBeanManager initStatelessBeanManager(PersistenceUtil persistenceUtil) {
-		if (PersistentBeanManager.getInstance().getPersistenceUtil() == null) {
-			PersistentBeanManager beanManager = PersistentBeanManager.getInstance();
-			beanManager.setPersistenceUtil(persistenceUtil);
-			beanManager.setProxyStore(new StatelessProxyStore());
-			beanManager.setClassMapper(null);
-		}
 
-		return PersistentBeanManager.getInstance();
-	}
+    /**
+     * Init bean manager for stateless mode
+     */
+    public static PersistentBeanManager initStatelessBeanManager(IPersistenceUtil persistenceUtil) {
+        if (PersistentBeanManager.getInstance().getPersistenceUtil() == null) {
+            PersistentBeanManager beanManager = PersistentBeanManager.getInstance();
+            beanManager.setPersistenceUtil(persistenceUtil);
+            beanManager.setProxyStore(new StatelessProxyStore());
+            beanManager.setClassMapper(null);
+        }
 
-	/**
-	 * Init bean manager for stateless mode for legacy Gilead 1.2 (encoded proxy info)
-	 */
-	public static PersistentBeanManager initLegacyStatelessBeanManager(PersistenceUtil persistenceUtil) {
-		if (PersistentBeanManager.getInstance().getPersistenceUtil() == null) {
-			PersistentBeanManager beanManager = PersistentBeanManager.getInstance(); // new PersistentBeanManager();
-			beanManager.setPersistenceUtil(persistenceUtil);
+        return PersistentBeanManager.getInstance();
+    }
 
-			StatelessProxyStore proxyStore = new StatelessProxyStore();
-			proxyStore.setProxySerializer(new JBossProxySerialization());
-			beanManager.setProxyStore(proxyStore);
+    /**
+     * Init bean manager for stateless mode for legacy Gilead 1.2 (encoded proxy info)
+     */
+    public static PersistentBeanManager initLegacyStatelessBeanManager(IPersistenceUtil persistenceUtil) {
+        if (PersistentBeanManager.getInstance().getPersistenceUtil() == null) {
+            PersistentBeanManager beanManager = PersistentBeanManager.getInstance(); // new PersistentBeanManager();
+            beanManager.setPersistenceUtil(persistenceUtil);
 
-			beanManager.setClassMapper(null);
-		}
+            StatelessProxyStore proxyStore = new StatelessProxyStore();
+            proxyStore.setProxySerializer(new JBossProxySerialization());
+            beanManager.setProxyStore(proxyStore);
 
-		return PersistentBeanManager.getInstance();
-	}
+            beanManager.setClassMapper(null);
+        }
 
-	/**
-	 * Init bean manager for stateful mode
-	 */
-	public static PersistentBeanManager initStatefulBeanManager(PersistenceUtil persistenceUtil) {
-		if (PersistentBeanManager.getInstance().getPersistenceUtil() == null) {
-			HttpSessionProxyStore proxyStore = new HttpSessionProxyStore();
-			proxyStore.setPersistenceUtil(persistenceUtil);
+        return PersistentBeanManager.getInstance();
+    }
 
-			PersistentBeanManager beanManager = PersistentBeanManager.getInstance(); // new PersistentBeanManager();
-			beanManager.setPersistenceUtil(persistenceUtil);
-			beanManager.setProxyStore(proxyStore);
-			beanManager.setClassMapper(null);
-		}
+    /**
+     * Init bean manager for stateful mode
+     */
+    public static PersistentBeanManager initStatefulBeanManager(IPersistenceUtil persistenceUtil) {
+        if (PersistentBeanManager.getInstance().getPersistenceUtil() == null) {
+            HttpSessionProxyStore proxyStore = new HttpSessionProxyStore();
+            proxyStore.setPersistenceUtil(persistenceUtil);
 
-		return PersistentBeanManager.getInstance();
-	}
+            PersistentBeanManager beanManager = PersistentBeanManager.getInstance(); // new PersistentBeanManager();
+            beanManager.setPersistenceUtil(persistenceUtil);
+            beanManager.setProxyStore(proxyStore);
+            beanManager.setClassMapper(null);
+        }
 
-	/**
-	 * Init bean manager for clone only mode
-	 */
-	public static PersistentBeanManager initBeanManagerForCloneOnly(PersistenceUtil persistenceUtil) {
-		if (PersistentBeanManager.getInstance().getPersistenceUtil() == null) {
-			PersistentBeanManager beanManager = PersistentBeanManager.getInstance();
-			beanManager.setPersistenceUtil(persistenceUtil);
-			beanManager.setProxyStore(new NoProxyStore());
-			beanManager.setClassMapper(null);
-		}
+        return PersistentBeanManager.getInstance();
+    }
 
-		return PersistentBeanManager.getInstance();
-	}
+    /**
+     * Init bean manager for clone only mode
+     */
+    public static PersistentBeanManager initBeanManagerForCloneOnly(IPersistenceUtil persistenceUtil) {
+        if (PersistentBeanManager.getInstance().getPersistenceUtil() == null) {
+            PersistentBeanManager beanManager = PersistentBeanManager.getInstance();
+            beanManager.setPersistenceUtil(persistenceUtil);
+            beanManager.setProxyStore(new NoProxyStore());
+            beanManager.setClassMapper(null);
+        }
+
+        return PersistentBeanManager.getInstance();
+    }
 }
